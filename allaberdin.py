19ca23dc723e7e7a2927b4218f9a7f8f53ec42ec
lzbonzo <lzbonzo@mail.ru>
2020-10-05 04:37:04 +0300

    Diploma. Always different asteroid. Need to fix endless exchange between drones

--- allaberdin.py
+++ allaberdin.py
@@ -7,30 +7,35 @@ from astrobox.core import Drone
 class AllaberdinDrone(Drone):
 
     def on_born(self):
-        self.available_asteroids = self.asteroids
         self.target = random.choice(self.asteroids)
         self.move_at(self.target)
 
     def on_stop_at_asteroid(self, asteroid):
         self.load_from(asteroid)
 
+    def on_overlap_with(self, obj_status):
+        if obj_status in self.teammates:
+            if 300 < self.distance_to(self.mothership):
+                if self.is_empty and obj_status.is_full:
+                    self.move_at(obj_status)
+                    obj_status.move_at(self)
+                    self.load_from(obj_status)
+                    # TODO решить проблему с бесконечным обменом между дронами
+
     def on_load_complete(self):
+        self.find_closest()
         if not self.is_full:
-            next_asteroid = self.find_closest()
-            if next_asteroid:
-                if self.distance_to(next_asteroid) < self.distance_to(self.my_mothership):
-                    self.move_at(next_asteroid)
-                    return
+            if self.distance_to(self.target) < self.distance_to(self.my_mothership):
+                self.move_at(self.target)
+                return
         self.move_at(self.my_mothership)
 
     def on_stop_at_mothership(self, mothership):
         self.unload_to(mothership)
 
     def on_unload_complete(self):
-        next_asteroid = self.find_closest()
-        if next_asteroid:
-            self.target = next_asteroid
-            self.move_at(self.target)
+        self.find_closest()
+        self.move_at(self.target)
 
     def find_closest(self):
         """
@@ -41,6 +46,7 @@ class AllaberdinDrone(Drone):
         for i in range(len(distances)):
             for asteroid in self.asteroids:
                 if self.distance_to(asteroid) == distances[i] and not asteroid.is_empty:
-                        if asteroid not in targets:
-                            self.target = asteroid
-                            return asteroid
+                    if asteroid not in targets:
+                        self.target = asteroid
+                        return
+        self.target = self.mothership
