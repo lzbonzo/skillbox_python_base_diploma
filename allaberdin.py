37f1a6c6d6375cf16dcd0e40de9adc884d0b0ae1
Alexander Nedovizin <parkit74@gmail.com>
2020-10-07 22:50:35 +0500

    Третий этап: зачёт!

--- allaberdin.py
+++ allaberdin.py
@@ -6,9 +6,6 @@ class AllaberdinDrone(Drone):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # self.full_charged_distance = 0
-        # self.not_empty_distance = 0
-        # self.empty_distance = 0
 
     def on_born(self):
         self.find_next()
@@ -28,8 +25,6 @@ class AllaberdinDrone(Drone):
 
     def on_unload_complete(self):
         self.find_next()
-        # if self.target == self.mothership:
-        #     self.print_stat()
         self.move_at(self.target)
 
     def find_next(self):
@@ -42,44 +37,6 @@ class AllaberdinDrone(Drone):
             asteroids = sorted(asteroids, key=lambda x: x.distance_to(self))
             if not self.is_full:
                 self.target = asteroids[0]
-                # self.sum_distance()
                 return
         if not self.near(self.mothership):
             self.target = self.mothership
-            # self.sum_distance()
-
-    # def sum_distance(self):
-    #     if self.is_full:
-    #         self.full_charged_distance += self.distance_to(self.target)
-    #     elif self.is_empty:
-    #         self.empty_distance += self.distance_to(self.target)
-    #     elif 0 < self.fullness < 1:
-    #         self.not_empty_distance += self.distance_to(self.target)
-    #
-    #
-    # def print_stat(self):
-    #     print(f'Дрон номер {self.id} пролетел:')
-    #     print(f'\t - Полностью загруженным: {self.full_charged_distance}')
-    #     print(f'\t - Загруженным не полностью: {self.not_empty_distance}')
-    #     print(f'\t - Пустым: {self.empty_distance}')
-    #
-    # def on_overlap_with(self, obj_status):
-    #     if obj_status in self.teammates:
-    #         if self.distance_to(self.mothership) > 150:
-    #             if not self.loaded:
-    #                 if self.free_space >= obj_status.payload:
-    #                     print('hey where')
-    #                     self.loaded = True
-    #                     self.move_at(obj_status)
-    #                     obj_status.move_at(self)
-    #                     self.load_from(obj_status)
-    #                     #
-    #                     self.find_next()
-    #                     self.move_at(self.target)
-    #                     obj_status.find_next()
-    #                     obj_status.move_at(obj_status.target)
-    #
-    #                     return
-    # - решить проблему с бесконечным обменом между дронами
-    # - Могу предложить сделать флаг дрона отдающий/принимающий. И в зависимости от него действовать.
-    #  Далее принявший сбрасывает флаг принимающего, когда разгрузился на базе
